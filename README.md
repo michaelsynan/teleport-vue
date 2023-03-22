3b07f0d61222b13d3f5bfa2999fdb5cb1bf24558
michaelsynan <93357198+michaelsynan@users.noreply.github.com>
2023-03-22 19:32:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# teleport-vue
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-starter-mhihpm)
---
+++ components/theModal.vue
@@ -0,0 +1,41 @@
+<template>
+  <transition name="modal">
+    <div class="modal-backdrop" @click="$emit('close')" v-if="isOpen">
+      <div class="modal-content" @click.stop>
+        <slot></slot>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    isOpen: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background-color: #fff;
+  padding: 1rem;
+  border-radius: 4px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+}
+</style>
